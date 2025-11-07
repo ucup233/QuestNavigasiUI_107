449c78193c0327047b0c184b5289af310edbc198
ucup233 <sufhidayat9@gmail.com>
2025-11-07 10:37:27 +0700

    Pembuatan variabel data untuk ditampilkan

--- app/src/main/java/com/example/week6/view/TampilData.kt
+++ app/src/main/java/com/example/week6/view/TampilData.kt
@@ -2,11 +2,18 @@ package com.example.week6.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.example.week6.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TampilData(
     onBackBtnClick: () -> Unit
 ) {
+    val items = listOf(
+        Pair(first = stringResource(id = R.string.nama_lengkap), second = "Contoh Nama"),
+        Pair(first = stringResource(id = R.string.jenis_kelamin), second = "Lainnya"),
+        Pair(first = stringResource(id = R.string.alamat), second = "Yogyakarta")
+    )
 
 }
