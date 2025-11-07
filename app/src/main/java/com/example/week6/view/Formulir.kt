6edfda51bf283eda66b3024e3914b2d4417480ad
ucup233 <sufhidayat9@gmail.com>
2025-11-07 09:44:04 +0700

    Pembuatan function FormIsian di file Formulir

---
+++ app/src/main/java/com/example/week6/view/Formulir.kt
@@ -0,0 +1,13 @@
+package com.example.week6.view
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormIsian(
+    jenisKelamin: List<String> = listOf("Laki-laki","Perempuan"),
+    OnSubmitBtnClick: () -> Unit
+){
+
+}
