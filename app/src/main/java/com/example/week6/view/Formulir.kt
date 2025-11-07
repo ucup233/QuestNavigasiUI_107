be8c0ce27dd958a725b66e778798dd4dd7d5187f
ucup233 <sufhidayat9@gmail.com>
2025-11-07 09:54:06 +0700

    Pembuatan TopAppBar

--- app/src/main/java/com/example/week6/view/Formulir.kt
+++ app/src/main/java/com/example/week6/view/Formulir.kt
@@ -1,7 +1,22 @@
 package com.example.week6.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.R
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -9,5 +24,13 @@ fun FormIsian(
     jenisKelamin: List<String> = listOf("Laki-laki","Perempuan"),
     OnSubmitBtnClick: () -> Unit
 ){
-
-}
+    Scaffold(modifier = Modifier,
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.home),
+                    color = Color.White)},
+                colors = TopAppBarDefaults.topAppBarColors
+                    (containerColor = colorResource(id = R.color.teal_700))
+            )
+        }
+    ){}
