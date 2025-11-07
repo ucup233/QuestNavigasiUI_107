65c4442c1594ed622c8e9a4baa9068c984bb08fc
ucup233 <sufhidayat9@gmail.com>
2025-11-07 10:44:56 +0700

    Pembuatan Button Back

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.compose.runtime)
+    implementation(libs.androidx.ui)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
