534980a3e5d98df6811e7ba95f03760fce906a56
ucup233 <sufhidayat9@gmail.com>
2025-11-07 13:24:03 +0700

    Pembenaran file MainActivity

--- app/src/main/java/com/example/week6/MainActivity.kt
+++ app/src/main/java/com/example/week6/MainActivity.kt
@@ -1,47 +1,28 @@
 package com.example.week6
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.week6.ui.theme.WEEK6Theme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            WEEK6Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            WEEK6Theme{
+                Scaffold(modifier = Modifier.fillMaxSize()){
+                    DataApp(modifier = Modifier)
                 }
             }
         }
     }
+
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WEEK6Theme {
-        Greeting("Android")
-    }
-}
