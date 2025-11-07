daa629deeaf88b1cf4ec19f816bafbd31109b880
ucup233 <sufhidayat9@gmail.com>
2025-11-07 10:38:53 +0700

    Pembuatan TopAppBar

--- app/src/main/java/com/example/week6/view/TampilData.kt
+++ app/src/main/java/com/example/week6/view/TampilData.kt
@@ -1,7 +1,13 @@
 package com.example.week6.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import com.example.week6.R
 
@@ -15,5 +21,21 @@ fun TampilData(
         Pair(first = stringResource(id = R.string.jenis_kelamin), second = "Lainnya"),
         Pair(first = stringResource(id = R.string.alamat), second = "Yogyakarta")
     )
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.tampil),
+                        color = Color.White
+                    )
+                },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = colorResource(id = R.color.teal_700)
+                )
+            )
+        }
+    ){
 
+    }
 }
