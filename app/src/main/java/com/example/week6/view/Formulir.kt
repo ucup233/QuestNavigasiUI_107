51855e38df402344c399f6b1d7030553c5130326
ucup233 <sufhidayat9@gmail.com>
2025-11-07 10:11:31 +0700

    Pembuatan Button Submit

--- app/src/main/java/com/example/week6/view/Formulir.kt
+++ app/src/main/java/com/example/week6/view/Formulir.kt
@@ -4,9 +4,11 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.DividerDefaults.Thickness
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
@@ -31,37 +33,45 @@ import androidx.compose.ui.unit.dp
 fun FormIsian(
     jenisK: List<String> = listOf("Laki-laki","Perempuan"),
     OnSubmitBtnClick: () -> Unit
-){
-    Scaffold(modifier = Modifier,
+) {
+    Scaffold(
+        modifier = Modifier,
         topBar = {
             TopAppBar(
-                title = { Text(text = stringResource(id = R.string.home),
-                    color = Color.White)},
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.home),
+                        color = Color.White
+                    )
+                },
                 colors = TopAppBarDefaults.topAppBarColors
                     (containerColor = colorResource(id = R.color.teal_700))
             )
         }
-    ){isiRuang ->
-        Column(modifier = Modifier.padding(isiRuang),
+    ) { isiRuang ->
+        Column(
+            modifier = Modifier.padding(isiRuang),
             verticalArrangement = Arrangement.Top,
-            horizontalAlignment = Alignment.CenterHorizontally) {
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
             OutlinedTextField(
                 value = "",
                 singleLine = true,
                 modifier = Modifier
                     .padding(top = 20.dp)
                     .width(width = 250.dp),
-                label = {Text(text = "Nama Lengkap")},
-                onValueChange = {},)
+                label = { Text(text = "Nama Lengkap") },
+                onValueChange = {},
+            )
         }
-        HorizontalDivider(modifier = Modifier
-            .padding(all = 20.dp)
-            .width(width = 250.dp), thickness = Thickness, color = Color.Red
+        HorizontalDivider(
+            modifier = Modifier
+                .padding(all = 20.dp)
+                .width(width = 250.dp), thickness = Thickness, color = Color.Red
         )
         Row {
-            jenisK.forEach {
-                    item->
-                Row(verticalAlignment = Alignment.CenterVertically){
+            jenisK.forEach { item ->
+                Row(verticalAlignment = Alignment.CenterVertically) {
                     RadioButton(
                         selected = false,
                         onClick = {}
@@ -70,17 +80,25 @@ fun FormIsian(
                 }
             }
         }
-        HorizontalDivider(modifier = Modifier
-            .padding(all = 20.dp)
-            .width(width = 250.dp), thickness = 1.dp, color = Color.Red
+        HorizontalDivider(
+            modifier = Modifier
+                .padding(all = 20.dp)
+                .width(width = 250.dp), thickness = 1.dp, color = Color.Red
         )
         OutlinedTextField(
             value = "",
             singleLine = true,
             modifier = Modifier
                 .width(width = 250.dp),
-            label = {Text(text = "Alamat")},
+            label = { Text(text = "Alamat") },
             onValueChange = {},
         )
         Spacer(modifier = Modifier.height(height = 30.dp))
+        Button(
+            modifier = Modifier.fillMaxWidth(fraction = 1f),
+            onClick = OnSubmitBtnClick
+        ) {
+            Text(text = stringResource(id = R.string.submit))
+        }
     }
+}
