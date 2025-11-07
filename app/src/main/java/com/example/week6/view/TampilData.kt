183ba70fe3226f5c9e875397b279fa01d0237a6c
ucup233 <sufhidayat9@gmail.com>
2025-11-07 10:34:49 +0700

    Pembuatan function TampilData

--- app/src/main/java/com/example/week6/view/TampilData.kt
+++ app/src/main/java/com/example/week6/view/TampilData.kt
@@ -1,2 +1,12 @@
 package com.example.week6.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TampilData(
+    onBackBtnClick: () -> Unit
+) {
+
+}
