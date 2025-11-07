8bd0aeffe5283d4c4c55975cda20d3695ea2273e
ucup233 <sufhidayat9@gmail.com>
2025-11-07 10:42:42 +0700

    Pembuatan Body TampilData

--- app/src/main/java/com/example/week6/view/TampilData.kt
+++ app/src/main/java/com/example/week6/view/TampilData.kt
@@ -36,6 +36,19 @@ fun TampilData(
             )
         }
     ){
-
-    }
+        items.forEach { item ->
+            Column {
+                Text(
+                    text = item.first.uppercase(),
+                    fontSize = 16.sp 
+                )
+                Text(
+                    text = item.second,
+                    fontWeight = FontWeight.Bold,
+                    fontFamily = FontFamily.Cursive,
+                    fontSize = 22.sp
+                )
+            }
+            HorizontalDivider(thickness = 1.dp, color = Color.Cyan)
+        }
 }
